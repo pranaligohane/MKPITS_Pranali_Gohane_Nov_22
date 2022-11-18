7526acc5e5c42197e0b96af8bdfdb63c70b0a00d
pranaligohane <118261475+pranaligohane@users.noreply.github.com>
2022-11-18 12:58:04 +0530

    additon of two no

---
+++ ADD.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num1,num2,result;
+clrscr();
+printf("enter the two integer no");
+scanf("%d%d%d",&num1,num2);
+result=num1+num2;
+printf("the sum of two no=%d",result);
+getch();
+}
