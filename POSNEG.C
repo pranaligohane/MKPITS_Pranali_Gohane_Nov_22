b8fb636b5d4fb9ed3b8cd2fd3d2498c223012117
pranaligohane <118261475+pranaligohane@users.noreply.github.com>
2022-11-20 22:37:01 +0530

    positive negative program.

---
+++ POSNEG.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+clrscr();
+printf("enter the no");
+scanf("%d",&num);
+if(num>0)
+{
+printf("the no is +ve");
+}
+else
+{
+printf("the no is -ve");
+}
+getch();
+}
