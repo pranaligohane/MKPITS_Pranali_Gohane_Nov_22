e2a7c95728d322f7916c54ac67adff5065100514
pranaligohane <118261475+pranaligohane@users.noreply.github.com>
2022-11-20 22:07:12 +0530

    area of circle.

---
+++ CIRCLE.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int r,area;
+clrscr();
+printf("enter radius of circle=");
+scanf("%d",&r);
+area=3.14*r*r;
+printf("area of circle=%d",area);
+getch();
+}
