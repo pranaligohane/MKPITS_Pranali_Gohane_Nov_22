6d53d22a733e58e966cc3fb84afeb718e56d4b22
pranaligohane <118261475+pranaligohane@users.noreply.github.com>
2022-11-20 22:05:28 +0530

    area of square.

---
+++ SQUARE.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int s,area;
+clrscr();
+printf("enter side of square=");
+scanf("%d",&s);
+area=s*s;
+printf("area of square=%d",area);
+getch();
+}
