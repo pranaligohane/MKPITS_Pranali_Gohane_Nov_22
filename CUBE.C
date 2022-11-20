9bc834c02594bf721a4f264b3e246cfe88442392
pranaligohane <118261475+pranaligohane@users.noreply.github.com>
2022-11-20 22:08:36 +0530

    cube program.

---
+++ CUBE.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,area;
+clrscr();
+printf("enter value of a=");
+scanf("%d",&a);
+area=4*a*a;
+printf("area of cube=%d",area);
+getch();
+}
