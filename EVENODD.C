07d34e7a3f81c22d25a1f3e1182c57b1693eb20e
pranaligohane <118261475+pranaligohane@users.noreply.github.com>
2022-11-20 22:33:50 +0530

    even odd program.

---
+++ EVENODD.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+clrscr();
+printf("enter the no.");
+scanf("%d",&num);
+if(num%5==0)
+{
+printf("the no is even");
+}
+else
+{
+print("the no is odd");
+}
+getch();
+}
