9d642928f7ed3a2d8e39fff7142b5d19cf3299f8
pranaligohane <118261475+pranaligohane@users.noreply.github.com>
2022-11-20 22:15:57 +0530

    area of rectangle.

---
+++ RECTANGL.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int length,width,area;
+clrscr();
+printf("enter the length and width=");
+scanf("%d%d",&length,&width);
+area=length*width;
+printf("area of reactangle= %d",area);
+getch();
+}
