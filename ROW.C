8d71d4c886f01fd5fdf0435323966d513950daa1
pranaligohane <118261475+pranaligohane@users.noreply.github.com>
2022-11-21 12:47:51 +0530

    row and column program.

---
+++ ROW.C
@@ -0,0 +1,17 @@
+ #include<stdio.h>
+ #include<conio.h>
+ void main()
+ {
+ int r,c;
+ clrscr();
+ for(r=1;r<=3;r++)
+ {
+ for(c=1;c<=4;c++)
+ {
+ printf("%d",r,c);
+ }
+ printf("\n");
+}
+getch();
+}
+
