ea07a3963b83a6aac1cf5784c1d2de216800f6a1
pranaligohane <118261475+pranaligohane@users.noreply.github.com>
2022-11-24 16:07:01 +0530

    void addition program.

---
+++ VOIDADD.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void add()
+{
+int a,b,res;
+printf("enter the number");
+scanf("%d %d",&a,&b);
+res=a+b;
+printf("the addition of two no=%d",res);
+}
+
+void main()
+{
+add();
+add();
+getch();
+}
