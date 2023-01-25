e540f5b46e5dd77b4d106db6addb3eca886a2750
Pranali Gohane <gohanepranali15@gmail.com>
2023-01-25 12:38:59 +0530

    String_demoexample

---
+++ Stringdemo01.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Stringdemo01
+{
+public static void main(String[] args)
+{
+
+String s = "Pranali";
+char ch;
+for(int i=0;i<7;i++)
+{
+ch=s.charAt(i);
+System.out.println(ch);
+}
+
+
+char data[]={'a','b','c','d','e','g','h'};
+String sob=new String(data);
+{
+
+System.out.println(sob);
+int len=sob.length();
+System.out.println(len);
+
+}
+}
+}
