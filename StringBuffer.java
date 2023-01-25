bca481f7db8f2cf2e24e5b4bb31c706c6fb3af44
Pranali Gohane <gohanepranali15@gmail.com>
2023-01-25 12:54:29 +0530

    StringBuffer.java

---
+++ StringBuffer.java
@@ -0,0 +1,14 @@
+public class String_Buffer{
+
+public static void main(String[] args) {
+
+StringBuffer sb=new StringBuffer("Pranali");
+System.out.println(sb);
+sb.reverse();
+System.out.println(sb);
+
+}
+}
+
+
+
