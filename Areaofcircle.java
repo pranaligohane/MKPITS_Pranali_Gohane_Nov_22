2ea39274f6ba24f178f475256bb5a976cc4c26cd
Pranali Gohane <gohanepranali15@gmail.com>
2023-01-25 13:13:24 +0530

    Areaofcircle.java

---
+++ Areaofcircle.java
@@ -0,0 +1,10 @@
+public class Areaofcircle{
+
+public static void main(String args[])
+{
+double pi=3.14,r=2.5,area;
+area=pi*r*r;
+System.out.println("Areaofcircle is");
+System.out.print(area);
+}
+}
