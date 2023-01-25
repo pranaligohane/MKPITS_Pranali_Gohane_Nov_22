e2079b37761f79ade3bdcb41cafcdb6e36433b0b
Pranali Gohane <gohanepranali15@gmail.com>
2023-01-25 13:28:31 +0530

    Concate.java

---
+++ Concate.java
@@ -0,0 +1,20 @@
+
+public class Concate
+{
+public static void main(String[] args)
+{
+
+String ob1="Ram";
+String ob2=new String();
+
+ob2=ob1;
+System.out.println(ob2);
+
+//using concate
+String ob3="Sita";
+System.out.println(ob1.concat(ob3));
+
+//using equal
+System.out.println(ob1.compareTo("Priya"));
+}
+}
