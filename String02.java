ed546d2adb296ce9b9c5eb6fa71ff6db18ee7a41
Pranali Gohane <gohanepranali15@gmail.com>
2023-01-27 12:46:52 +0530

    String02.java

---
+++ String02.java
@@ -0,0 +1,17 @@
+public class one_one{
+
+//reverse
+public static void main(String[] args) {
+
+StringBuffer sb=new StringBuffer("Pranali");
+System.out.println(sb);
+
+sb.reverse();
+System.out.println(sb);
+
+
+}
+}
+
+
+
