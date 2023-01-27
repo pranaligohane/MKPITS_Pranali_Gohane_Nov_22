c2668554e265340bc6e9614d82a53ffa9f09c637
Pranali Gohane <gohanepranali15@gmail.com>
2023-01-27 11:59:41 +0530

    Multiplication03.java

---
+++ Multiplication03.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Multiplication03
+{
+public static void main(String[] args)
+{
+
+int n1,n2,n3,num;
+Scanner sc=new Scanner(System.in);
+
+System.out.println("Enter your 1 number");
+n1=sc.nextInt();
+
+System.out.println("Enter your 2  number");
+n2=sc.nextInt();
+
+System.out.println("Enter your 3 number");
+n3=sc.nextInt();
+
+num=n1*n2*n3;
+System.out.println("Multiplication03 * " +num);
+}
+
+}
+
+
