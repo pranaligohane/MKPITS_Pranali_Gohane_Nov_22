cc572f4e58907b1dc420cdeca9a8ed4e26328cad
Pranali Gohane <gohanepranali15@gmail.com>
2023-01-27 13:10:49 +0530

    triangle.java

---
+++ triangle.java
@@ -0,0 +1,10 @@
+class triangle
+{
+public static void main(String[] args)
+{
+int base=3,height=5;
+double c=0.5*base*height;
+System.out.println("enter the value base,height");
+System.out.println("area of triangle="+c);
+}
+}
