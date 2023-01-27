70b5a9f4c0948ab24747b42f9b09df0f2e5be510
Pranali Gohane <gohanepranali15@gmail.com>
2023-01-27 12:15:01 +0530

    one1.java

---
+++ one1.java
@@ -0,0 +1,16 @@
+class arraydemo{
+void fun(){
+int a[]={1,2,3,4};
+int[]a1;
+for(int i=0;i<=3;i++)
+{
+System.out.println(a[i]);
+}
+}
+}
+public class one1{
+public static void main(String[]args){
+arraydemo ob=new arraydemo();
+ob.fun();
+}
+}
