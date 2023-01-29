1e4245ccff45d0ca7cd8f6fed22c9580d6a43817
Pranali Gohane <gohanepranali15@gmail.com>
2023-01-29 15:16:01 +0530

    AssignmentQue15.java

---
+++ AssignmentQue15.java
@@ -0,0 +1,27 @@
+/*Write a program to check whether two given integers are in the range 40..50 inclusive, or they are both in the range 50..60 inclusive.*/
+
+import java.util.Scanner;
+
+class AssignmentQue15
+{
+public static void main(String[] args)
+{
+
+Scanner sc=new Scanner(System.in);
+System.out.print("Enter any two numbers: ");
+int a=sc.nextInt();
+int b=sc.nextInt();
+System.out.println("a = "+a);
+System.out.println("b = "+b);
+
+if((a>=40 && a<=50 && b>=40 && b<=50) || (a>=50 && a<=60 && b>=50 && b<=60))
+{
+System.out.println("The entered number is in range");
+}
+else
+{
+System.out.println("The entered number is not in range");
+}
+
+}
+}
