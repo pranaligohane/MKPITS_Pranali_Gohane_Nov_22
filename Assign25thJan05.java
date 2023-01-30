599c47e4c53a87e6061365e254060ec491ee8f30
Pranali Gohane <gohanepranali15@gmail.com>
2023-01-30 17:39:22 +0530

    Assign25thJan05.java

---
+++ Assign25thJan05.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Assign25thJan05
+{
+public static void main(String[] args)
+{
+
+int [] arr = {2,5,4,6,8,9,3};
+int [] arrNew = {arr[3],arr[4],arr[5],arr[6]};
+
+System.out.print("arrNew [] : ");
+for(int i = 0; i<arrNew.length; i++)
+{
+System.out.print(arrNew[i]+" ");
+
+}
+}
+}
