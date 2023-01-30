73f72be8b749023417cb1f13f4abbcfdaf260d3d
Pranali Gohane <gohanepranali15@gmail.com>
2023-01-30 22:42:41 +0530

    Assign25thJan07.java

---
+++ Assign25thJan07.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Assign25thJan07
+{
+public static void main(String[] args)
+{
+
+int a[]={4,6,9};
+if(a[0]>a[1] && a[0]>a[2])
+{
+
+System.out.println("The largest array is "+a[0]);
+}
+if(a[1]>a[0] && a[1]>a[2])
+{
+
+System.out.println("The largest array is "+a[1]);
+}
+if(a[2]>a[0] && a[2]>a[1])
+{
+
+System.out.println("The largest array is "+a[2]);
+
+}
+}
+}
+
+
