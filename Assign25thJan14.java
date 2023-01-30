5093a6b0ec7cf35b1c3e5bceecb52042cdb76b91
Pranali Gohane <gohanepranali15@gmail.com>
2023-01-30 23:22:41 +0530

    Assign25thJan14.java

---
+++ Assign25thJan14.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Assign25thJan14
+{
+public static void main(String[] args)
+{
+
+System.out.print("Enter the number");
+Scanner sc=new Scanner(System.in);
+int num=sc.nextInt();
+if(num<=3)
+{
+System.out.println("The number is greater" +num);
+}
+else
+{
+System.out.println("The number is not greater" +num);
+
+}
+}
+}
