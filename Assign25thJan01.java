0092859466e30ae2d79b44a3b58c564a36bf648b
Pranali Gohane <gohanepranali15@gmail.com>
2023-01-30 14:31:43 +0530

    Assign25thJan01.java

---
+++ Assign25thJan01.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Assign25thJan01
+{
+public static void main(String[] args)
+{
+
+int a[]={1,2,3};
+int b[]={4,5,6};
+int sum1,sum2;
+
+sum1=(a[0]+a[1]+a[2]);
+System.out.println("Addition of first array=" +sum1);
+sum2=(b[0]+b[1]+b[2]);
+System.out.println("Addition of second array=" +sum2);
+
+if(sum1>sum2)
+{
+System.out.println("sum1 is greater=" +sum1);
+}
+else
+{
+System.out.println("sum2 is greater=" +sum2);
+}
+}
+}
+
