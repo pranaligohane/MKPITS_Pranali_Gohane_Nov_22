bef92aee38ae4b5e89f932290fa2575ea5e555d7
Pranali Gohane <gohanepranali15@gmail.com>
2023-01-31 16:42:43 +0530

    Throws_Demo1.java

---
+++ Throws_Demo1.java
@@ -0,0 +1,11 @@
+public class Throws_Demo1
+{
+public static void main(String[] args) throws Exception
+{
+
+System.out.println("Start Program");
+Thread.sleep(1000);
+System.out.println("End Program");
+
+}
+}
