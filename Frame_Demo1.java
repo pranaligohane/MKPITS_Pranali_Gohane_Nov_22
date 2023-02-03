310d5b56edf64d7938be2129454381108d5d1331
Pranali Gohane <gohanepranali15@gmail.com>
2023-02-03 16:46:10 +0530

    Frame_Demo1.java

---
+++ Frame_Demo1.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+
+class Frame1 extends Frame
+{
+Frame1()
+{
+
+setVisible(true);
+}
+}
+
+public class Frame_Demo1{
+public static void main(String[] args){
+new Frame1();
+
+}
+}
